be61717763735ad1567cf329503b5c830100ecfb
Jeffrey W. Helkenberg <j.w.helkenberg@gmail.com>
2019-02-04 22:56:18 -0600

    Add files via upload

---
+++ A173593.py
@@ -0,0 +1,52 @@
+
+a=3
+b=5
+c=6
+d=11
+e=13
+f=22
+g=27
+h=45
+i=54
+j=91
+k=109
+l=182
+
+for x in range(0,100):
+  print(a)
+  a = a+(216*64**x)
+  print(b)
+  b = b+(360*64**x)
+  print(c)
+  c = c+(432*64**x)
+  print(d)
+  d = d+(720*64**x) 
+  print(e)
+  e = e+(864*64**x)
+  print(f)   
+  f = f+(1440*64**x) 
+  print(g)
+  g = g+(1728*64**x)
+  print(h)
+  h = h+(2880*64**x)
+  print(i)
+  i = i+(3456*64**x)
+  print(j)
+  j = j+(5760*64**x)
+  print(k) 
+  k = k+(6912*64**x)
+  print(l)
+  l = l+(11520*64**x)
+
+
+
+
+
+
+
+
+
+
+
+
+
